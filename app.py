57f00f22fe4c2f082fb3b48c401dacd5258d2be2
KahaanKapasi <kahaan2608@gmail.com>
2026-03-30 10:40:39 +0530

    DB integration

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import sqlite3  
+import os
+import psycopg2
+from dotenv import load_dotenv
 
-db_name = "bank.db"
+load_dotenv()
+db_url = os.getenv("DATABASE_URL")
 
 def db_init():
-    conn = sqlite3.connect(db_name)
+    conn = psycopg2.connect(db_url)
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id SERIAL PRIMARY KEY,
             name TEXT NOT NULL,
             balance REAL DEFAULT 0,
             password TEXT NOT NULL
@@ -33,9 +36,9 @@ def login():
     name = input("Enter Username: ")
     passw = input("Enter Password: ")
     
-    conn = sqlite3.connect(db_name)
+    conn = psycopg2.connect(db_url)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE name=? AND password=?", (name, passw))
+    cursor.execute("SELECT * FROM users WHERE name=%s AND password=%s", (name, passw))
     user_data = cursor.fetchone()
     conn.close()
 
@@ -50,9 +53,9 @@ def createAccount():
     n = input("Enter Name: ")
     p = input("Enter Password: ")
     
-    conn = sqlite3.connect(db_name)
+    conn = psycopg2.connect(db_url)
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (name, password) VALUES (?, ?)", (n, p))
+    cursor.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (n, p))
     conn.commit()
     conn.close()
     
@@ -61,7 +64,7 @@ def createAccount():
 
 def loginPage():
     db_init()
-    print("1 for Login\n2 for Sign in")
+    print("1 for Login\n2 for Sign Up")
     try:
         c = int(input("Enter choice: "))
         if c == 1:
@@ -72,6 +75,8 @@ def loginPage():
             print("Invalid Choice")
     except ValueError:
         print("Please enter a valid number.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     loginPage()
